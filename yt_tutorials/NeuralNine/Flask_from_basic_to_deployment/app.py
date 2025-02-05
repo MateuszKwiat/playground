8187e05e576b3201d832148eaa23fca07a3b12cc
MateuszKwiat <mateusz.kwiat316@gmail.com>
2025-02-05 13:43:44 +0100

    part three of tutorial

--- yt_tutorials/NeuralNine/Flask_from_basic_to_deployment/app.py
+++ yt_tutorials/NeuralNine/Flask_from_basic_to_deployment/app.py
@@ -1,31 +1,32 @@
-from flask import Flask, request
+from flask import Flask, render_template, redirect, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!<h1>'
+    my_list = [10, 20, 30, 40, 50]
+    return render_template('index.html', mylist=my_list)
 
-@app.route('/hello')
-def hello():
-    return 'Hello World!'
+@app.route('/other')
+def other():
+    some_text = 'Hello World!'
+    return render_template('other.html', some_text=some_text)
 
-@app.route('/greet/<name>')
-def greet(name):
-    return f'Hello {name}!'
+@app.route('/redirect_endpoint')
+def redirect_endpoint():
+    return redirect(url_for('other'))
 
-@app.route('/add/<int:number1>/<int:number2>')
-def add(number1, number2):
-    return f'{number1} + {number2} = {number1 + number2}'
+@app.template_filter('reverse_string')
+def reverse_string(s):
+    return s[::-1]
 
-@app.route('/handle_url_params')
-def handle_params():
-    if 'greeting' in request.args.keys() and 'name' in request.args.keys():
-        greeting = request.args.get('greeting')
-        name = request.args.get('name')
-        return f'{greeting}, {name}'
-    else:
-        return 'some params are missing :('
+@app.template_filter('repeat')
+def repeat(s, times=2):
+    return s * times
+
+@app.template_filter('alternate_case')
+def alternate_case(s):
+    return ''.join([ch.upper() if i % 2 == 0 else ch.lower() for i, ch in enumerate(s)])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
